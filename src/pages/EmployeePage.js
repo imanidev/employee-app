5cfbabe7b560b9ff83603b5d90d50b41565e895c
imani.devs <imaniroberts.dev@gmail.com>
2023-05-18 20:29:01 -0400

    added files

---
+++ src/pages/EmployeePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function EmployeePage() {
+    return (
+        <div>
+            <h1>Employee Page</h1>
+            {/* Add your employee page content here */}
+        </div>
+    );
+}
+
+export default EmployeePage;
